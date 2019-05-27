772be1c169ec6b3885358302680f77698a824f2b
Alfonso Gonzalez <trilogyti@gmail.com>
2019-05-27 13:02:26 -0500

    Add the Article controller and the modifications of show.html.twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,8 +4,9 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -21,8 +22,19 @@ class ArticleController
     public function show($slug)
     {
         //return new Response('Use the function show for a new page url');
-        return new Response(sprintf(
-            'Future page to show the article: %s',
-            $slug));
+        // return new Response(sprintf(
+        //     'Future page to show the article: %s',
+        //     $slug));
+
+        $comments = [
+            'Me encantan los tacos!',
+            'Los de suadero son lo mejor',
+            'Todos los tacos son ricos!!'
+        ];
+
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+        ]);
     }
 }
